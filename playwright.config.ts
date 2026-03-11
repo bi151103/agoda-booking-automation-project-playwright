cd68f56bec875d79118f29e4d473deda1c2ed650
bi151103 <dangphanminhphuctbag@gmail.com>
2026-03-11 16:38:54 +0700

    create playwright.config.ts

---
+++ playwright.config.ts
@@ -0,0 +1,43 @@
+import { defineConfig, devices } from '@playwright/test';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export default defineConfig(
+  {
+    tsconfig: './tsconfig.json',
+
+    fullyParallel: true,//if true, all the tests within a test file or suite will run in parallel
+    //we cannot determine the order of files running in parallel
+    retries: process.env.CI ? 2 : undefined,
+    reporter: 'html',
+    workers: process.env.CI ?? 10,
+
+    testDir: './tests',
+
+    //timeout
+    globalTimeout: 30 * 60 * 60,//30m, default none
+    expect: {
+      timeout: 0//default 5s
+    },
+    timeout: 90 * 1000,//90s, default 30s
+
+    //common options for all tests
+    use: {
+      actionTimeout: 0,//default none
+      navigationTimeout: 0,// default none
+      baseURL: process.env.BASE_URL,
+      trace: 'on-first-retry',
+      serviceWorkers: 'block'//for data intercepting
+    },
+
+    //project-specific config
+    projects: [
+      {
+        name: 'chrome-desktop-project',
+        testMatch: ['tests/desktop/chrome/**.ts'],
+        use: devices['Desktop Chrome']
+      }
+    ]
+  }
+); 
