503666c546fa9d9a25ecb21b519ae37c65298b49
bi151103 <dangphanminhphuctbag@gmail.com>
2026-03-13 09:15:22 +0700

    feat: add page class for search page

--- pages/searchPage.ts
+++ pages/searchPage.ts
@@ -1,8 +1,22 @@
-import { type Page } from "@playwright/test";
+import { type Locator, type Page } from "@playwright/test";
 import BasePage from "./basePage";
 
 export default class SearchPage extends BasePage {
     constructor(page: Page) {
         super(page);
+    }
+
+    async closeOccupancyDialog(): Promise<void> {
+        if (await this.occupancyDialogEle().isVisible()) {
+            await this._page.locator("#occupancy-box").click();
+        }
+    }
+
+    async selectPropertyFromThePropertiesList(index: number): Promise<void> {
+        await this._page.locator("ol.hotel-list-container li.PropertyCard.PropertyCardItem").nth(index).locator("a[target='_blank']").click();
+    }
+
+    occupancyDialogEle(): Locator {
+        return this._page.getByRole("dialog", { name: "Occupancy selection" });
     }
 }
